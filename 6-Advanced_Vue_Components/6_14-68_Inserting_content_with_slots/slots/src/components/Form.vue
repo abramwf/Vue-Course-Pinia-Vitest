cf40c343362bc3ca495bf0f7d0200ce35a8a6c96
abramwf <kemocengbersih@gmail.com>
2024-08-21 20:11:25 +0700

    add new 7

---
+++ 6-Advanced_Vue_Components/6_14-68_Inserting_content_with_slots/slots/src/components/Form.vue
@@ -0,0 +1,17 @@
+<template>
+    <form action="">
+        <slot>
+            No form to render
+        </slot>
+        <div class="help">
+            <slot name="help"></slot>
+        </div>
+        <div class="fields">
+            <slot name="fields"></slot>
+        </div>
+        <div class="buttons">
+            <slot name="buttons"></slot>
+        </div>
+        <slot></slot>
+    </form>
+</template>
