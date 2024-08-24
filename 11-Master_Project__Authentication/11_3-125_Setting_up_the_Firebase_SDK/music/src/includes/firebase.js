071e514139dbc2e4fac42ee91e966e10392a0291
abramwf <kemocengbersih@gmail.com>
2024-08-24 21:28:27 +0700

    finish authentication

---
+++ 11-Master_Project__Authentication/11_3-125_Setting_up_the_Firebase_SDK/music/src/includes/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmNifimYxIIXbQmKLfG-UvcSPjnFGAQS0',
+  authDomain: 'music-28c3c.firebaseapp.com',
+  projectId: 'music-28c3c',
+  storageBucket: 'music-28c3c.appspot.com',
+  //   messagingSenderId: '1070051593612',
+  appId: '1:1070051593612:web:4658c221dff2a683819aa9'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+const usersCollection = db.collection('users')
+
+export { auth, db, usersCollection }
