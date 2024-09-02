f4d33c3770b44b8ca3313bd14af9d98485146b88
abramwf <kemocengbersih@gmail.com>
2024-09-02 10:55:19 +0700

    finish all

---
+++ 22-Component_Design_Patterns/22_2-268_Controlled_Components/src/components/EmailInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <input type="email" class="form-control" placeholder="E-mail" :value="email" @input="update($event)" :class="{
+        'is-valid': validateEmail(email),
+        'is-invalid': !validateEmail(email),
+    }" />
+</template>
+
+<script>
+import validateEmail from '../validate-email.js'
+
+export default {
+    name: 'EmailInput',
+    props: ['email'],
+    methods: {
+        update($event) {
+            this.$emit('update:modelValue', $event.target.value)
+        },
+        validateEmail
+    }
+}
+</script>
